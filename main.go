2cf8123f594cb9d7af3adfb6f7352d6c042d5625
Luis Vargas <luisvargasr10@gmail.com>
2022-03-11 21:13:55 -0600

    FIRST COMMIT

---
+++ main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	server := NewServer(":300")
+}
---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import "net/http"
+
+type Server struct {
+	port string
+}
+
+// Create a new Server instance
+func NewServer(port string) *Server {
+	return &Server{
+		port: port,
+	}
+}
+
+func (s *Server) Listen() error {
+	err := http.ListenAndServe(s.port, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
